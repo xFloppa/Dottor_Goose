df54de4c8f1653ac3f4ef586eafda4c2373ad1ea
xFloppa <floppa.strunz1234@gmail.com>
2024-05-21 17:51:59 +0200

    test

---
+++ Dottor_Goose_Game/Dottor_Goose_Game/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Dottor_Goose_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPlayer = new System.Windows.Forms.PictureBox();
+            this.btnLancioDado = new System.Windows.Forms.Button();
+            this.btnResetGame = new System.Windows.Forms.Button();
+            this.btnQuitGame = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(962, 582);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBoxPlayer
+            // 
+            this.pictureBoxPlayer.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.pictureBoxPlayer.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxPlayer.Image")));
+            this.pictureBoxPlayer.Location = new System.Drawing.Point(73, 22);
+            this.pictureBoxPlayer.Name = "pictureBoxPlayer";
+            this.pictureBoxPlayer.Size = new System.Drawing.Size(50, 68);
+            this.pictureBoxPlayer.TabIndex = 1;
+            this.pictureBoxPlayer.TabStop = false;
+            // 
+            // btnLancioDado
+            // 
+            this.btnLancioDado.Location = new System.Drawing.Point(48, 647);
+            this.btnLancioDado.Name = "btnLancioDado";
+            this.btnLancioDado.Size = new System.Drawing.Size(75, 23);
+            this.btnLancioDado.TabIndex = 2;
+            this.btnLancioDado.Text = "Dado";
+            this.btnLancioDado.UseVisualStyleBackColor = true;
+            this.btnLancioDado.Click += new System.EventHandler(this.btnLancioDado_Click);
+            // 
+            // btnResetGame
+            // 
+            this.btnResetGame.Location = new System.Drawing.Point(329, 615);
+            this.btnResetGame.Name = "btnResetGame";
+            this.btnResetGame.Size = new System.Drawing.Size(75, 23);
+            this.btnResetGame.TabIndex = 3;
+            this.btnResetGame.Text = "Reset Game";
+            this.btnResetGame.UseVisualStyleBackColor = true;
+            this.btnResetGame.Click += new System.EventHandler(this.btnResetGame_Click);
+            // 
+            // btnQuitGame
+            // 
+            this.btnQuitGame.Location = new System.Drawing.Point(917, 615);
+            this.btnQuitGame.Name = "btnQuitGame";
+            this.btnQuitGame.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitGame.TabIndex = 4;
+            this.btnQuitGame.Text = "Esci";
+            this.btnQuitGame.UseVisualStyleBackColor = true;
+            this.btnQuitGame.Click += new System.EventHandler(this.btnQuitGame_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(437, 620);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 13);
+            this.labelResult.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 613);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 682);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.btnQuitGame);
+            this.Controls.Add(this.btnResetGame);
+            this.Controls.Add(this.btnLancioDado);
+            this.Controls.Add(this.pictureBoxPlayer);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Dottor Goose Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxPlayer;
+        private System.Windows.Forms.Button btnLancioDado;
+        private System.Windows.Forms.Button btnResetGame;
+        private System.Windows.Forms.Button btnQuitGame;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Dottor_Goose_Game/Dottor_Goose_Game/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dottor_Goose_Game
+{
+    public partial class Form1 : Form
+    {
+        
+       
+           
+             Point(81, 93),   // 1
+             Point(81, 166),  // 2
+            new Point(81, 237),  // 3
+            new Point(81, 308),  // 4
+            new Point(81, 377),  // 5
+            new Point(81, 449),  // 6
+            new Point(196, 449), // 7
+            new Point(308, 449), // 8
+            new Point(313, 378), // 9
+            new Point(313, 308), // 10
+            new Point(313, 238), // 11
+            new Point(313, 166), // 12
+            new Point(313, 93),  // 13
+            new Point(440, 93),  // 14
+            new Point(563, 93),  // 15
+            new Point(699, 93),  // 16
+            new Point(818, 93), // 17
+
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        private int playerPosition = 0;
+
+        private Point[] boardPositions = new Point[28];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            boardPositions[0] = new Point(81, 23);
+            boardPositions[1] = new Point();
+            boardPositions[2] = new Point();
+            boardPositions[3] = new Point();
+            boardPositions[4] = new Point();
+            boardPositions[5] = new Point();
+            boardPositions[6] = new Point();
+            boardPositions[7] = new Point();
+            boardPositions[8] = new Point();
+            playerPosition = 0;
+            pictureBoxPlayer.Location = boardPositions[0];
+
+            btnLancioDado.Text = "Tira i dadi";
+            btnLancioDado.Click += new EventHandler(btnLancioDado_Click);
+
+            labelResult.Text = "Risultato dei dadi: ";
+        }
+
+        private void btnLancioDado_Click(object sender, EventArgs e)
+        {
+            
+            int diceResult = random.Next(1, 7);
+            textBox1.Text =  diceResult.ToString();
+
+            MovePlayer(diceResult);
+
+            
+        }
+
+        private void MovePlayer(int diceResult)
+        {
+            playerPosition += diceResult;
+
+
+            /* if (playerPosition >= boardPositions.Length)
+            {
+                playerPosition = boardPositions.Length - 1;
+            }*/
+
+            pictureBoxPlayer.Location = boardPositions[playerPosition];
+        }
+
+        private void btnQuitGame_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnResetGame_Click(object sender, EventArgs e)
+        {
+            playerPosition = 0;
+            pictureBoxPlayer.Location = boardPositions[0];
+        }
+    }
+}
